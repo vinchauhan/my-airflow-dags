0f5231a60029c80682e38e74df948a33760bd898
vchauhan <chauhan.vineet@gmail.com>
2021-01-17 00:33:21 -0700

    Working DAG

--- csv-parser.py
+++ csv-parser.py
@@ -7,7 +7,7 @@ from datetime import date
 
 @dag(default_args={'owner': 'airflow'}, schedule_interval=None, start_date=days_ago(2))
 def my_test_dag():
-   @task
+   @task()
    def fetch_data_to_local():
         url = "https://data.cityofnewyork.us/resource/rc75-m7u3.json"
         response = requests.get(url)
@@ -18,15 +18,8 @@ def my_test_dag():
         df = df.set_index("date_of_interest")
 
     # for integrity reasons, let's attach the current date to the filename
-        df.to_csv("/home/vchauhan/nyccovid_{}.csv".format(date.today().strftime("%Y%m%d")))
-        return df
-
-#    @task
-#    def display_records(data):
-#         print(f'Converted JSON is : {data}')
-
-   @task()
-   def run():
-    local_data = fetch_data_to_local()
-
+        df.to_csv("/Users/vchauhan/nyccovid_{}.csv".format(date.today().strftime("%Y%m%d")))
+        #return df
+   fetch_data_to_local()
+   
 tutorial_etl_dag = my_test_dag()
